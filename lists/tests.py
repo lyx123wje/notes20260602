243f7a8ba4fc82f55592d5148b606d9cf260b95e
LY <2678621488@qq.com>
2026-04-13 10:18:19 +0800

    message 4: First unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page # (1) 从我们的视图里导入主页函数
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3) 
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/') # (2) 解析根路径 URL
+        self.assertEqual(found.func, home_page) # (3) 检查解析到的函数是否是 home_page
+
+
+
+
+
