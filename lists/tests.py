7d4b2b5c295ae5e75fe973f866b46a99801f4ec1
LY <2678621488@qq.com>
2026-04-27 09:27:12 +0800

    message 10: First model migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,31 @@
+from calendar import firstweekday
 import html
 from urllib import response
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page # (1) 从我们的视图里导入主页函数
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.models import Item
 
-class HomePageTest(TestCase):
+
+class ItemModelTest(TestCase):
+    def test_save_and_retrieve_items(self):
+        first_item=Item()
+        first_item.text='The first list item'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(second_saved_item.text, second_item.text)
 
     def test_uses_home_template(self):
         response=self.client.get('/')
